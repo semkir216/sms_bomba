474ee28a36c8c796cbfe96ce0fd6a5010affd4ba
Elnur Babayev <35178745+semkir216@users.noreply.github.com>
2020-11-05 00:09:51 +0400

    Add files via upload

---
+++ termux.sh
@@ -0,0 +1,9 @@
+echo "Lazımlı fayllar yüklənir."
+pkg install python
+pkg install python3
+pkg install python3-pip
+pip install requests
+pip install colorama
+echo "Fayllar yükləndi."
+python3 bomber.py
+
